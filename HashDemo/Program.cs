02f53ad451989622cecfec46287cc3a4fafa2714
mohan338 <sahumohan338@gmail.com>
2022-05-24 17:33:55 +0530

    [mohan]add:Find the frequency of word in phrase

--- HashDemo/Program.cs
+++ HashDemo/Program.cs
@@ -4,32 +4,79 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        HashMap<string, int> hash = new HashMap<string, int>(5);
-        string words = "to be or not to be";
-        string[] arr = words.Split(' ');
-        LinkedList<string> checkForDuplication = new LinkedList<string>();
-        foreach (string element in arr) // to -> be
+       HashMap<string, int> hash = new HashMap<string, int>(5);//size 5
+        Console.WriteLine("Welcome to Hash Table");
+        bool end = true;
+        Console.WriteLine("1.Frequency Of Words in sentence \n2. Frequency Of Words In Large Paragraph Phrase \n 3.End the Program");
+        while (end == true)
         {
-            int count = 0;
-            foreach (string match in arr) //to->be->or->not->to-> be-> 
+            Console.WriteLine("Take an option to execute");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
             {
-                if (element == match)
-                {
-                    count++;//1->2
-                    if (checkForDuplication.Contains(element))
+                case 1:
+                    string words = "to be or not to be";
+                    string[] arr = words.Split(' ');
+                    LinkedList<string> checkForDuplication = new LinkedList<string>();
+                    foreach (string element in arr) // to -> be
                     {
-                        break;
+                        int count = 0;
+                        foreach (string match in arr) //to->be->or->not->to-> be-> 
+                        {
+                            if (element == match)
+                            {
+                                count++;//1->2
+                                if (checkForDuplication.Contains(element))
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                        if (!checkForDuplication.Contains(element))
+                        {
+                            checkForDuplication.AddLast(element);
+                            hash.Add(element, count);//(to,2)
+
+                        }
                     }
-                }
+                    hash.Display();
+                    break;
+                case 2:
+                    string phrase = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+                    string[] arr1 = phrase.Split(' ');
+                    LinkedList<string> checkDuplication = new LinkedList<string>();
+                    foreach (string element in arr1)
+                    {
+                        int count = 0;
+                        foreach (string match in arr1)
+                        {
+                            if (element == match)
+                            {
+                                count++;
+                                if (checkDuplication.Contains(element))
+                                {
+                                    break;
+                                }
+                            }
 
-            }
-            if (!checkForDuplication.Contains(element))
-            {
-                checkForDuplication.AddLast(element);
-                hash.Add(element, count);//(to,2)
+                        }
 
+                        if (checkDuplication.Contains(element))
+                        {
+                            continue;
+                        }
+                        checkDuplication.AddLast(element);
+                        hash.Add(element, count);
+                    }
+                    hash.Display();
+                    break;
+                case 3:
+                    end = false;
+                    break;
+                default:
+                    Console.WriteLine("Enter Proper Option To Execute");
+                    break;
             }
         }
-        hash.Display();
     }
 }
